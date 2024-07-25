5d096d3917f7774692472daa07c502f4ad50dc9f
Bram Bolt <bram.bolt@ru.nl>
2024-07-25 14:29:39 +0200

    updated init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,5 @@
 from .image_utils import load_image, save_image
 from .map_utils import read_map, get_coords, get_array
 from .image_processing import apply_overlay, apply_shadows
+from .avatar_generator import generate_avatar
 from .cli import parse_arguments, main
